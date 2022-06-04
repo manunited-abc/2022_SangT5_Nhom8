2d17d1a075f68fcea965f9cc9020cf740e203528
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2022-06-04 15:31:48 +0700

    init sign in

--- src/main/java/nlu/nhcnpm/nhom8/entity/User.java
+++ src/main/java/nlu/nhcnpm/nhom8/entity/User.java
@@ -6,4 +6,46 @@ public class User {
     String email;
     String phoneNumber;
     String password;
+
+    public User() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
