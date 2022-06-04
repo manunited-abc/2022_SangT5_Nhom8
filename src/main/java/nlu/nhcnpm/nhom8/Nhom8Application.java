256c747675229f769468c1eb36b14343016495b9
AlanWalker <19130071@st.hcmuaf.edu.vn>
2022-06-04 16:42:57 +0700

    fix error not load resource

--- src/main/java/nlu/nhcnpm/nhom8/Nhom8Application.java
+++ src/main/java/nlu/nhcnpm/nhom8/Nhom8Application.java
@@ -3,7 +3,7 @@ package nlu.nhcnpm.nhom8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class Nhom8Application {
 
     public static void main(String[] args) {
