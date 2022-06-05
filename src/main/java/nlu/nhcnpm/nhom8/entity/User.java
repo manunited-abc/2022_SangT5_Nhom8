3ef181d1f0e1243aa7b59be4d3b326b4fd7da93d
AlanWalker <19130071@st.hcmuaf.edu.vn>
2022-06-05 11:37:40 +0700

    update code

--- src/main/java/nlu/nhcnpm/nhom8/entity/User.java
+++ src/main/java/nlu/nhcnpm/nhom8/entity/User.java
@@ -1,5 +1,14 @@
 package nlu.nhcnpm.nhom8.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     int id;
     String name;
@@ -7,45 +16,4 @@ public class User {
     String phoneNumber;
     String password;
 
-    public User() {}
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
