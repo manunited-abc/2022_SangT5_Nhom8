5fab1ae5487ebd696a9ffb19ea2fc5082c637af3
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2022-06-06 10:47:17 +0700

    sign-in ajax

--- src/main/java/nlu/nhcnpm/nhom8/controller/SignInController.java
+++ src/main/java/nlu/nhcnpm/nhom8/controller/SignInController.java
@@ -1,9 +1,12 @@
 package nlu.nhcnpm.nhom8.controller;
 
 import nlu.nhcnpm.nhom8.entity.User;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 
 @Controller
 public class SignInController {
@@ -14,12 +17,13 @@ public class SignInController {
 
     @PostMapping(value="checkSignIn")
     public String checkSignIN(@ModelAttribute User user, Model model) {
-        System.out.println(user.getEmail() +" "+user.getPassword());
         if (!user.getEmail().equals("e")) {
             model.addAttribute("emailValidation", "email is not exist");
+            return "signIn :: email-validation";
         }
         else if (!user.getPassword().equals("p")) {
             model.addAttribute("passwordValidation", "password is wrong");
+            return "signIn :: password-validation";
         }
         model.addAttribute("user", user);
         return "signIn";
