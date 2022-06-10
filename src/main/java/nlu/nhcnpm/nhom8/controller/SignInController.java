8ad9260d7c2a9f19297c72ffd5943d8560a68238
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2022-06-10 21:39:14 +0700

    update encrypt password

--- src/main/java/nlu/nhcnpm/nhom8/controller/SignInController.java
+++ src/main/java/nlu/nhcnpm/nhom8/controller/SignInController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
 @Controller
@@ -21,11 +24,32 @@ public class SignInController {
         if (!user.getEmail().equals("e")) {
             model.addAttribute("emailValidation", "email is not exist");
             return "signIn :: email-validation";
-        } else if (!user.getPassword().equals("p")) {
+        }
+        String encryptPassword = encryptPassword(user.getPassword());
+        if (!encryptPassword.equals("p")) {
             model.addAttribute("passwordValidation", "password is wrong");
             return "signIn :: password-validation";
         }
         model.addAttribute("user", user);
         return "signIn";
+    }
+
+    private String encryptPassword(String password) {
+        String encryptPassword = password;
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(password.getBytes());
+            byte[] bytes = m.digest();
+            StringBuilder s = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            encryptPassword = s.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encryptPassword;
     }
 }
